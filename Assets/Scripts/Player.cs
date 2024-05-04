700acb62a8a53dd94ed427872710db7424908865
Furkan Safa Altunyuva <furkansafaaltunyuva@gmail.com>
2024-05-04 13:45:42 +0300

    Rain Control with Space Key

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,45 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    void Start()
+    [SerializeField] public float acids = 100;
+    [SerializeField] private ParticleSystem rain;
+    [SerializeField] private GameObject rainParticleSystemGameObject;
+    public float initialEmissionRate = 0;
+
+    private void Start()
     {
+        rain = rainParticleSystemGameObject.GetComponent<ParticleSystem>();
+        initialEmissionRate = rain.emission.rateOverTime.constant;
         
+        var emission = rain.emission;
+        emission.rateOverTime = 0;
     }
 
-    
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space)) // Check if space is pressed 
+        {
+            if (acids > 0) // Check if there are acids left
+            {
+                acids -= 1 * Time.deltaTime; // Decrease acids
+                //rain.Play(); // Play particle system (Not working for some reason)
+                var emission = rain.emission;
+                emission.rateOverTime = initialEmissionRate;
+            }
+            else
+            {
+                //rain.Stop(); // Stop particle system (Not working for some reason)
+                var emission = rain.emission;
+                emission.rateOverTime = 0;
+            }
+        }
+        else
+        {
+            // rain.Stop(); // Stop particle system (Not working for some reason)
+            var emission = rain.emission;
+            emission.rateOverTime = 0;
+        }
     }
 
     private void OnParticleCollision(GameObject other)
