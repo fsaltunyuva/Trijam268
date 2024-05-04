e9aab04bfc75c12a6c54870504f6f7c290a2c14d
Furkan Safa Altunyuva <furkansafaaltunyuva@gmail.com>
2024-05-04 13:56:28 +0300

    Draggable Cloud

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour
+{
+    Vector3 mousePositionOffset;
+    
+    private Vector3 GetMouseWorldPosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    
+    private void OnMouseDown()
+    {
+        mousePositionOffset = gameObject.transform.position - GetMouseWorldPosition();
+    }
+    
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition() + mousePositionOffset;
+    }
+}
